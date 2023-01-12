647f3efeffdf7635876f8c49cbd3f3a3735e3553
xuxx <bhxxx@aliyun.com>
2023-01-12 20:38:51 +0800

    版本0.2

--- qzkj/admin.py
+++ qzkj/admin.py
@@ -4,7 +4,7 @@ from .models import User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('nick_name', 'telephone', 'introducer', 'score_nowithdraw', 'score_withdrawable')
+    list_display = ('nick_name', 'telephone', 'user_name', 'introducer', 'score_nowithdraw', 'score_withdrawable')
     readonly_fields = ('user_name', 'nick_name', 'telephone', 'introducer',)
     search_fields = ('nick_name', 'telephone')
 
