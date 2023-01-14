e8cd762b5ce827fc8b2e185675354ebc650c504b
xuxx <bhxxx@aliyun.com>
2023-01-14 17:36:45 +0800

    .

--- qzkj/views.py
+++ qzkj/views.py
@@ -7,13 +7,13 @@ from django.views import View
 from qzkj.models import User
 from django.core.exceptions import ObjectDoesNotExist
 import requests
-
+import qzkj.settings
 
 logger = logging.getLogger('log')
 
 
 def verify(request):
-    with open('MP_verify_yxSL8Vl2Cy7VfcHP.txt', 'r') as f:
+    with open(settings.BASE_DIR + 'MP_verify_yxSL8Vl2Cy7VfcHP.txt', 'r') as f:
         str = f.readline()
     return HttpResponse(str)
 
