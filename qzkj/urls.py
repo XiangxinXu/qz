35bba4e8824669d6d825fc9106e2b8c5e5edf630
xuxx <bhxxx@aliyun.com>
2023-02-11 10:05:13 +0800

    .

--- qzkj/urls.py
+++ qzkj/urls.py
@@ -36,7 +36,7 @@ urlpatterns = [
 
     # 用户授权相关
     re_path(r'^(/)?$', TemplateView.as_view(template_name='user_auth.html')), #请求code
-    re_path(r'^user_auth(/)?$', TemplateView.as_view(template_name='user_auth.html'))
+    re_path(r'^user_auth(/)?$', TemplateView.as_view(template_name='user_auth.html')),
     re_path(r'^get_access_token', views.get_accesstoken), #请求access token
 
 
