183b7256f758b3c6c81cfc3cc517ff497bfc66b7
xuxx <bhxxx@aliyun.com>
2023-02-14 13:21:23 +0800

    .

--- qzkj/views.py
+++ qzkj/views.py
@@ -64,7 +64,7 @@ def get_accesstoken(request):
         responsedict = json.loads(response.text)
         logger.info(response.text)
         
-        return JsonResponse(render_to_string('register.html', context=responsedict))
+        return JsonResponse(render_to_string('register.html', context=responsedict), safe=False)
 
 
 class UserView(View):
