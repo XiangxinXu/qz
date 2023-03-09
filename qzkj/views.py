9a4d5657a23ab2693b01c0d6f398065d694d2dbf
xuxx <bhxxx@aliyun.com>
2023-03-09 22:02:03 +0800

    a

--- qzkj/views.py
+++ qzkj/views.py
@@ -58,7 +58,7 @@ def get_user_info_from_wx(openid, access_token):
 
 
 def get_user_info_from_db(openid):
-    user = User.objects.get(user_name=openid)  
+    user = User.objects.get(openid=openid)  
     logger.info(user.nick_name)   
     ctx = {'nickname': user.nick_name, 'uscore': user.score_nowithdraw+user.score_withdrawable}   
     return ctx
