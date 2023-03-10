4b1ba88d650fd2eb90953a3aeeef071b6f11aad6
xuxx <bhxxx@aliyun.com>
2023-03-10 09:12:05 +0800

    .

--- qzkj/models.py
+++ qzkj/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    user_name = models.CharField(max_length=200, primary_key=True, verbose_name='微信id')
+    openid = models.CharField(max_length=200, primary_key=True, verbose_name='微信id')
     nick_name = models.CharField(max_length=200, blank=True, verbose_name='微信昵称')
     telephone = models.CharField(max_length=20, null=False, blank=False, verbose_name='手机号')
     introducer= models.ForeignKey('self', null=True, default=None, on_delete=models.SET_DEFAULT, verbose_name='介绍人')
@@ -13,3 +13,18 @@ class User(models.Model):
     class Meta:
         verbose_name_plural = '芪尊用户'
         db_table = 'User'
+
+class ScoreChangeLog(models.Model):
+    openid = models.CharField(max_length=200, primary_key=True, verbose_name='微信id')
+    change_datetime = models.DateTimeField(auto_now=True, verbose_name='时间')
+    score_change = models.IntegerField(default=0, verbose_name='积分变动')
+
+    class ScoreTypeChoices(models.TextChoices):
+        BOTH = 'both'
+        NO = 'nowithdraw'
+        WI = 'withdrawable'
+    score_type = models.CharField(max_length=15, choices = ScoreTypeChoices.choices, verbose_name='可否提现')
+    
+    class Meta:
+        verbose_name_plural = '积分变动详情'
+        db_table = 'ScoreChangeLog'
